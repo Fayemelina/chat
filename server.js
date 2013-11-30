7b2658de2d8af39ef1e14b039e75e757e79ca967
Fayemelina <fayehgreen@gmail.com>
2013-11-30 17:27:39 +0000

    Added mongodb

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-var express = require("express");
+var fs = require('fs');
+var mongo = require('mongodb');
+
+var mongoose = require('mongoose');  
+
+var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI
+
+mongoose.connect(connectionString);
+var express = require('express');
+var config = JSON.parse(fs.readFileSync('config.json'));
+var host = config.host;
+var port = config.port;
+var dbPort = mongo.Connection.DEFAULT_PORT;
+var db = new mongo.Db('jobbies', new mongo.Server(host, dbPort, {}));
+
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var userCollection;
+
+
+app.get('/', function(req, res){
+	var content = fs.readFileSync('user.html');
+
+	getUsers(function(users){
+		var ul = '';
+		users.forEach(function(user){
+			ul += '<li>' + user.name + ':' + user.email + '</li>';
+		});
+		content = content.toString('utf8').replace('{{USERS}}', ul);
+		res.setHeader('Content-Type', 'text/html');
+		res.send(content);
+	});
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+db.open(function(error){
+	db.collection('user', function(error, collection){
+		userCollection = collection;
+	});
 });
+
+function getUsers(callback) {
+	var coll = userCollection.find().toArray();
+	callback(coll);
+};
+
+app.listen(port, host);
